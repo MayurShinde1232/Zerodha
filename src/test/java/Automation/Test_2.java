c8572ab60327b4c2eea50e60ddc11a9e56cb0fb8
MayurShinde1232 <mayurjayavantshinde1@gmail.com>
2025-09-15 21:24:23 +0530

    Navigation code

--- src/test/java/Automation/Test_2.java
+++ src/test/java/Automation/Test_2.java
@@ -7,16 +7,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Test_2 {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		
 		ChromeDriver driver= new ChromeDriver();
 		
-		driver.get("https://www.facebook.com/");
+		//driver.get("https://www.facebook.com/");
+		
+		driver.navigate().to("https://www.facebook.com/");
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.close();
 		
 		
 		
+		
+		
+		 
 	}
 
 }
